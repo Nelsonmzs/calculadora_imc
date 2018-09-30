1dbfb83567e560ff9e4546752c184b660965ad59
Nelsonmzs <nelson30_8@hotmail.com>
2018-09-29 23:59:54 -0300

    - Melhorias

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
 
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  String _titleApp = "Calculadora de IMC";
+  String _calcularButton = "Calcular";
   String _infoText = "Informe seus dados";
 
   void _resetFields() {
@@ -55,7 +56,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calculadora de IMC"),
+        title: Text(_titleApp),
         centerTitle: true,
         backgroundColor: Colors.lightBlueAccent,
         actions: <Widget>[
@@ -72,11 +73,11 @@ class _HomeState extends State<Home> {
         padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
         child: Form(
           key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Icon(Icons.person, size: 120.0, color: Colors.lightBlueAccent),
-                TextFormField(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person, size: 120.0, color: Colors.lightBlueAccent),
+              TextFormField(
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                       labelText: "Peso (kg)",
@@ -86,11 +87,10 @@ class _HomeState extends State<Home> {
                   controller: weightController,
                   validator: (value) {
                     if (value.isEmpty) {
-                      return "Informe seu Peso!";
+                      return "Informe o Peso!";
                     }
-                  }
-                ),
-                TextFormField(
+                  }),
+              TextFormField(
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                       labelText: "Altura (cm)",
@@ -98,35 +98,34 @@ class _HomeState extends State<Home> {
                   textAlign: TextAlign.center,
                   style: TextStyle(color: Colors.black38, fontSize: 25.0),
                   controller: heightController,
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return "Informe sua Altura!";
-                      }
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Informe a Altura!";
                     }
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: Container(
-                    height: 50.0,
-                    child:  RaisedButton(
-                      onPressed: () {
-                        if (_formKey.currentState.validate()) {
-                          _calculate();
-                        }
-                      },
-                      child: Text("Calcular",
-                          style: TextStyle(color: Colors.white, fontSize: 25.0)),
-                      color: Colors.lightBlueAccent,
-                    ),
+                  }),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _calculate();
+                      }
+                    },
+                    child: Text(_calcularButton,
+                        style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                    color: Colors.lightBlueAccent,
                   ),
                 ),
-                Text(
-                  _infoText,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.black38, fontSize: 20.0),
-                )
-              ],
-            ),
+              ),
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black38, fontSize: 20.0),
+              )
+            ],
+          ),
         ),
       ),
     );
